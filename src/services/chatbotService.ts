921bf07ee634e54b0c7240db3c4aef1c970c5aa6
EliasRosendahl <jensen.eliasrosendahl@gmail.com>
2021-05-03 13:00:53 +0100

    Basic watson integration

--- src/services/chatbotService.ts
+++ src/services/chatbotService.ts
@@ -1,10 +1,43 @@
 import { Service } from 'typedi';
+import dotenv from 'dotenv';
+
+dotenv.config();
+
+
+const AssistantV2 = require('ibm-watson/assistant/v2');
+const { IamAuthenticator } = require('ibm-watson/auth');
+
+const assistant = new AssistantV2({
+    version: '2021-05-03',
+    authenticator: new IamAuthenticator({
+        apikey: process.env.WATSON_API_KEY,
+    }),
+    serviceUrl: 'https://api.eu-de.assistant.watson.cloud.ibm.com',
+});
+
+assistant.createSession({
+    assistantId: process.env.ASSISTANT_ID
+  })
+    .then((res: { result: any; }) => {
+      console.log(JSON.stringify(res.result, null, 2));
+    })
+    .catch((err: any) => {
+      console.log(err);
+    });
 
 
 @Service()
 export class ChatbotService {
-    constructor() {}
     public async dialog(question: string): Promise<string> {
-        return "test123";
+        const response = await assistant.messageStateless({
+            assistantId: process.env.ASSISTANT_ID,
+            input: {
+              'message_type': 'text',
+              'text': question,
+            }
+        });
+
+        const resposeChatText = response.result.output.generic[0].text; 
+        return resposeChatText;
     }
 }
